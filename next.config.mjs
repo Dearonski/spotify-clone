1ae338b366b735f971f1dbc994b918f2bf2e5b7b
Dearonski <abromovich546@gmail.com>
2024-03-12 22:29:15 +0300

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ["opjxceljhavumovgirfo.supabase.co"],
+    },
+};
 
 export default nextConfig;
