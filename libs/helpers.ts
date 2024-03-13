84840556ef0a5d16af2adc60b2bc7b1399ae5b63
Dearonski <abromovich546@gmail.com>
2024-03-13 12:20:44 +0300

    fix getURL

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -3,7 +3,7 @@ import { Price } from "@/types";
 export const getURL = () => {
     let url =
         process.env.NEXT_PUBLIC_SITE_URL ??
-        process.env.NEXT_PUBLIC_VERSEL_URL ??
+        process.env.NEXT_PUBLIC_VERCEL_URL ??
         "http://localhost:3000/";
 
     url = url.includes("http") ? url : `https://${url}`;
